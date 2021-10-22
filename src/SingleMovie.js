a0f1f8a02d6b68de8960a2b02156fb2518a71ee7
luisluft <luberluft@gmail.com>
2021-10-22 08:35:01 -0400

    implement react router

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { API_ENDPOINT } from "./context";
+console.log("API_ENDPOINT :", API_ENDPOINT);
 
 const SingleMovie = () => {
   return <h2>single movie</h2>;
